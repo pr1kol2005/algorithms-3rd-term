804fca1d70afaebb2f63771249a43177a59e1e74
cyril grinko <k.a.grinko@gmail.com>
2024-09-24 11:58:29 +0300

    changed dynamic states array's name

--- 1st contest/J.cpp
+++ 1st contest/J.cpp
@@ -19,19 +19,22 @@ void PrintLCS(const std::vector<size_t>& lcs_first,
 size_t FindLCS(const std::string& first, const std::string& second,
                std::vector<size_t>& lcs_first,
                std::vector<size_t>& lcs_second) {
-  Matrix<size_t> dp(first.size() + 1,
-                    std::vector<size_t>(second.size() + 1, 0));
+  Matrix<size_t> dp_lcs_of_prefixes(first.size() + 1,
+                                    std::vector<size_t>(second.size() + 1, 0));
   Matrix<Pair> prev(first.size() + 1, std::vector<Pair>(second.size() + 1));
 
   for (size_t i = 1; i <= first.size(); i++) {
     for (size_t j = 1; j <= second.size(); j++) {
       if (first[i - 1] == second[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
+        dp_lcs_of_prefixes[i][j] = dp_lcs_of_prefixes[i - 1][j - 1] + 1;
         prev[i][j] = Pair(i - 1, j - 1);
       } else {
-        dp[i][j] = std::max(dp[i][j - 1], dp[i - 1][j]);
+        dp_lcs_of_prefixes[i][j] = std::max(dp_lcs_of_prefixes[i][j - 1],
+                                            dp_lcs_of_prefixes[i - 1][j]);
         prev[i][j] =
-            (dp[i][j - 1] >= dp[i - 1][j]) ? Pair(i, j - 1) : Pair(i - 1, j);
+            (dp_lcs_of_prefixes[i][j - 1] >= dp_lcs_of_prefixes[i - 1][j])
+                ? Pair(i, j - 1)
+                : Pair(i - 1, j);
       }
     }
   }
@@ -45,14 +48,14 @@ size_t FindLCS(const std::string& first, const std::string& second,
       lcs_second.push_back(l - 1);
       --k;
       --l;
-    } else if (dp[k - 1][l] > dp[k][l - 1]) {
+    } else if (dp_lcs_of_prefixes[k - 1][l] > dp_lcs_of_prefixes[k][l - 1]) {
       --k;
     } else {
       --l;
     }
   }
 
-  return dp[first.size()][second.size()];
+  return dp_lcs_of_prefixes[first.size()][second.size()];
 }
 
 int main() {
