2ab274f6e92aa7d633574b2adf5d15f53cd5f475
cyril grinko <k.a.grinko@gmail.com>
2024-09-24 03:00:26 +0300

    corrected on request

--- 1st contest/I.cpp
+++ 1st contest/I.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-void PrintAnswer(std::vector<int>& dp, std::vector<int>& blocks) {
+void PrintAnswer(const std::vector<int>& dp, const std::vector<int>& blocks) {
   std::cout << dp[blocks.size()] << '\n';
   for (size_t i = 1; i < blocks.size(); i++) {
     if (dp[i] < dp[i + 1]) {
@@ -11,28 +11,27 @@ void PrintAnswer(std::vector<int>& dp, std::vector<int>& blocks) {
   std::cout << *(blocks.end() - 1) << '\n';
 }
 
-void CountConqueredBlocks(std::vector<int>& blocks) {
-  std::vector<int> dp_1(blocks.size() + 1, 1);
-  std::vector<int> dp_2(blocks.size() + 1, 1);
+std::vector<int> CountConqueredBlocks(const std::vector<int>& blocks) {
+  std::vector<int> dp_less_more(blocks.size() + 1, 1);
+  std::vector<int> dp_more_less(blocks.size() + 1, 1);
 
   for (size_t i = 2; i <= blocks.size(); i++) {
     for (size_t j = i - 1; j >= 1; --j) {
-      if ((dp_1[j] % 2 == 0 && blocks[j - 1] > blocks[i - 1]) ||
-          (dp_1[j] % 2 != 0 && blocks[j - 1] < blocks[i - 1])) {
-        dp_1[i] = std::max(dp_1[j] + 1, dp_1[i]);
+      if ((dp_less_more[j] % 2 == 0 && blocks[j - 1] > blocks[i - 1]) ||
+          (dp_less_more[j] % 2 != 0 && blocks[j - 1] < blocks[i - 1])) {
+        dp_less_more[i] = std::max(dp_less_more[j] + 1, dp_less_more[i]);
       }
-      if ((dp_2[j] % 2 == 0 && blocks[j - 1] < blocks[i - 1]) ||
-          (dp_2[j] % 2 != 0 && blocks[j - 1] > blocks[i - 1])) {
-        dp_2[i] = std::max(dp_2[j] + 1, dp_2[i]);
+      if ((dp_more_less[j] % 2 == 0 && blocks[j - 1] < blocks[i - 1]) ||
+          (dp_more_less[j] % 2 != 0 && blocks[j - 1] > blocks[i - 1])) {
+        dp_more_less[i] = std::max(dp_more_less[j] + 1, dp_more_less[i]);
       }
     }
   }
 
-  if (dp_1[blocks.size()] > dp_2[blocks.size()]) {
-    PrintAnswer(dp_1, blocks);
-  } else {
-    PrintAnswer(dp_2, blocks);
+  if (dp_less_more[blocks.size()] > dp_more_less[blocks.size()]) {
+    return dp_less_more;
   }
+  return dp_more_less;
 }
 
 int main() {
@@ -45,5 +44,5 @@ int main() {
     std::cin >> blocks[i];
   }
 
-  CountConqueredBlocks(blocks);
+  PrintAnswer(CountConqueredBlocks(blocks), blocks);
 }
