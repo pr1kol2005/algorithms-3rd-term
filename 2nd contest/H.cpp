9471ce9787f60333ede5dae3e537517548f23e2a
cyril grinko <k.a.grinko@gmail.com>
2024-10-03 18:15:53 +0300

    solution preparation

--- 2nd contest/H.cpp
+++ 2nd contest/H.cpp
@@ -1,50 +1,31 @@
 #include <iostream>
 #include <vector>
 
-using Matrix = std::vector<std::vector<int>>;
+template <typename T>
+using Matrix = std::vector<std::vector<T>>;
 
-struct CircularIndex{
-  int value;
-  int size;
-
-  CircularIndex(int v, int s) : value(v), size(s) {}
-
-  CircularIndex& operator++() {
-    value = (value + 1) % size;
-    return *this;
+void InputMatrix(Matrix<int>& adj_matrix) {
+  for (size_t i = 0; i < adj_matrix.size(); ++i) {
+    std::string row;
+    std::cin >> row;
+    for (size_t j = 0; j < row.size(); ++j) {
+      adj_matrix[i][j] = row[j] - '0';
+    }
   }
-
-  CircularIndex& operator--() {
-    value = (value + size - 1) % size;
-    return *this;
-  }
-
-  CircularIndex operator+(int n) const {
-    return CircularIndex((value + n) % size, size);
-  }
-
-  CircularIndex operator-(int n) const {
-    return CircularIndex((value - n + size) % size, size);
-  }
-};
-
-
-void SolveProblem(const std::vector<int64_t>& missions) {
-  return;
 }
 
+int64_t CountCliques(const Matrix<int>& adj_matrix) {
+  return 0;
+}
 
 int main() {
   int n = 0;
-  std::vector<int64_t> missions(n, 0);
 
   std::cin >> n;
 
-  for (int i = 0; i < n; i++) {
-    std::cin >> missions[i];
-  }
+  Matrix<int> adj_matrix(n, std::vector<int>(n, 0));
 
-  std::cout << -2 % 3 << '\n';
+  InputMatrix(adj_matrix);
 
-  // SolveProblem(missions);
+  std::cout << CountCliques(adj_matrix) << '\n'; 
 }
