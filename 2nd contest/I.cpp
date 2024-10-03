e8857b9d65b786336646a8b25b25218a5a42f98b
cyril grinko <k.a.grinko@gmail.com>
2024-10-03 18:04:07 +0300

    solution preparation

---
+++ 2nd contest/I.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+int CountDistributions(const std::string& n, int m, int mod) {
+  return 0;
+}
+
+int main() {
+  std::string n;
+  int m = 0;
+  int mod = 0;
+
+  std::cin >> n;
+  std::cin >> m;
+  std::cin >> mod;
+
+  std::cout << CountDistributions(n, m, mod) << '\n';
+}
